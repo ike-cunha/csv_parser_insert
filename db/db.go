66d2e2b4f71aaf00e7dc320ffc7b866909bfa404
Henrique Cunha <hcunha@exactsales.com.br>
2021-04-10 22:01:59 -0300

    Store createTable command

--- db/db.go
+++ db/db.go
@@ -1,6 +1,11 @@
 package db
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
 
 const CONNECTION_STRING = "user=docker password=docker dbname=neoway host=db sslmode=disable"
 
@@ -14,4 +19,27 @@ type Store struct {
 	LojaMaisFrequente  string
 	LojaUltimaCompra   string
 	Invalido           bool
+}
+
+//Creates Store table
+func createTable(db *sql.DB) {
+	create := `CREATE TABLE IF NOT EXISTS "store"  (
+		"id" SERIAL PRIMARY KEY,
+		"cpf" varchar(20),
+		"privado" boolean,
+		"incompleto" boolean,
+		"data_ultima_compra" date,
+		"ticket_medio" decimal,
+		"ticket_ultima_compra" decimal,
+		"loja_mais_frequente" varchar(20),
+		"loja_ultima_compra" varchar(20),
+		"invalido" boolean
+	  );`
+
+	result, err := db.Exec(create)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result.RowsAffected())
 }
